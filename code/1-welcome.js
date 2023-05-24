48af66fadc608ed085f1f7d50f56f95830e5f62f
Hyunji <124632128+kimhy27@users.noreply.github.com>
2023-05-24 22:56:00 +0000

    welcome page

--- code/1-welcome.js
+++ code/1-welcome.js
@@ -15,5 +15,19 @@ function welcome() {
 }
 
 function questionnaire() {
-  // write the code for your questionnaire here
+  let name = prompt("what's your name?")
+  alert('What a cool name, ' + name + '!')
+
+  let breakfast = prompt('What did you have for breakfast, ' + name + '?')
+  alert('Wow, ' + breakfast + ' sounds delicious!')
+
+  let colour = prompt ('What is your favourite colour, ' + name + '?')
+  alert('I like ' + colour + ' too!')
+
+  let age = prompt ('How old are you, ' + name + '?')
+  age = Number(age)
+  age = age+1
+  alert ('You will be ' + age + ' years old on your next birthday!')
+
+  
 }
