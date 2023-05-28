14c1310a742f3bd608352f21acf727d8001b354d
Hyunji <124632128+kimhy27@users.noreply.github.com>
2023-05-28 21:52:33 +0000

    mihi

--- code/2-maori-mihi.js
+++ code/2-maori-mihi.js
@@ -7,13 +7,19 @@ function maoriMihi() {
   alert('This program will help you build a basic mihi.')
   alert("First, you'll be asked a series of questions, then your mihi will be ready.")
 
-  alert('What is your name?')
-  let name = prompt()
-  // more questions and answers here
+  let name = prompt('What is your name?')
+  let mountain = prompt('What is your maunga?')
+  let river = prompt('What is your awa?')
+  let hometown = prompt('What is your town?')
 
+
+ 
+  // more outputs here 
   alert('Your mihi:')
-  // more outputs here
-
+  alert('Tēnā koutou katoa,')
+  alert('Ko ' + mountain + ' tōku maunga,')
+  alert('Ko ' + river + ' tōku awa,')
+  alert('Nō ' + hometown + ' au,')
   alert('Ko ' + name + ' tōku ingoa,')
   alert('Tēnā koutou,')
   alert('Tēnā koutou,')
