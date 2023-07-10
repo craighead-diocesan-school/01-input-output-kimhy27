b88165585c2de6986a9b6a01afb463dff70324c3
Hyunji <124632128+kimhy27@users.noreply.github.com>
2023-07-10 21:09:46 +0000

    non maori mihi

--- code/3-non-maori-mihi.js
+++ code/3-non-maori-mihi.js
@@ -7,12 +7,19 @@ function nonMaoriMihi() {
   alert('This program will help you build a basic mihi.')
   alert("First, you'll be asked a series of questions, then your mihi will be ready.")
 
-  alert('What is your name?')
-  let name = prompt()
+  let name = prompt('What is your name?')
+  let mountain = prompt('What is your maunga?')
+  let river = prompt('What is your awa?')
+  let hometown = prompt('What is your town?')
 
   alert('Your mihi:')
-  // more outputs here
-
+ 
+  alert('Tēnā koutou,')
+  alert('Kei te mihi au ki a ' + mountain )
+  alert('Kei te mihi au ki a ' + river )
+  alert('Ko Te Triti o Waitangi te waka,')
+  alert('Ko Aotearoa te iwi')
+  alert('Nō ' + hometown + ' au,')
   alert('Ko ' + name + ' tōku ingoa,')
   alert('Tēnā koutou,')
   alert('Tēnā koutou,')
